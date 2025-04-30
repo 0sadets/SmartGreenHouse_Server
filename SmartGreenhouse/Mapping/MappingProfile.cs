d6969648801b7ef901a99a9acb1816cd5ad04edb
0sadets <102857086+0sadets@users.noreply.github.com>
2025-04-30 14:25:39 +0300

    Add Mapping

---
+++ SmartGreenhouse/Mapping/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SmartGreenhouse.Models.DTOs;
+using SmartGreenhouse.Models.Entities;
+
+namespace SmartGreenhouse.Mapping
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile() {
+
+            CreateMap<GreenhouseCreateDto, Greenhouse>();
+            CreateMap<GreenhouseUpdateDto, Greenhouse>();
+            CreateMap<Greenhouse, GreenhouseReadDto>();
+
+
+        }
+    }
+}
