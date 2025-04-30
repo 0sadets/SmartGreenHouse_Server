f9f110567b645fda9bb666083644a72c86f5fa5b
0sadets <102857086+0sadets@users.noreply.github.com>
2025-04-30 23:43:01 +0300

    add repository

---
+++ SmartGreenhouse/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+namespace SmartGreenhouse.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        // CRUD operation 
+
+        IEnumerable<TEntity> Get(
+           Expression<Func<TEntity, bool>> filter = null,
+           Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+           params string[] includeProperties);
+
+        TEntity GetById(object id);
+        void Create(TEntity entity);
+        void Update(TEntity entityToUpdate);
+        void Delete(object id);
+        void Delete(TEntity entityToDelete);
+        void Save();
+    }
+}
---
+++ SmartGreenhouse/Repository.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartGreenhouse.Interfaces;
+using System.Linq.Expressions;
+
+namespace SmartGreenhouse
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        internal AppDbContext context;
+        internal DbSet<TEntity> dbSet;
+        public Repository(AppDbContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<TEntity>();
+        }
+
+        public void Create(TEntity entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public virtual void Delete(object id)
+        {
+            TEntity entityToDelete = dbSet.Find(id);
+            if (entityToDelete == null)
+                throw new ArgumentException("Entity not found.");
+            Delete(entityToDelete);
+        }
+
+        public virtual void Delete(TEntity entityToDelete)
+        {
+            if (context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                dbSet.Attach(entityToDelete);
+            }
+            dbSet.Remove(entityToDelete);
+        }
+
+        public virtual IEnumerable<TEntity> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+             params string[] includeProperties)
+        {
+            IQueryable<TEntity> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties)
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+
+      
+
+        public TEntity GetById(object id)
+        {
+            return dbSet.Find(id);
+        }
+
+    
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void Update(TEntity entityToUpdate)
+        {
+            dbSet.Attach(entityToUpdate);
+            context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+    }
+}
