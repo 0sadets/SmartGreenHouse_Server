7f1477bb5ae854362b327f1c2df9780a6c276829
0sadets <102857086+0sadets@users.noreply.github.com>
2025-04-30 14:22:07 +0300

    Add greenhouseDto, validation

---
+++ SmartGreenhouse/Models/DTOs/GreenhouseUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿namespace SmartGreenhouse.Models.DTOs
+{
+    public class GreenhouseUpdateDto
+    {
+        public string Name { get; set; }
+        public float Length { get; set; }
+        public float Width { get; set; }
+        public float Height { get; set; }
+        public string Season { get; set; }
+        public string Location { get; set; }
+    }
+
+}
---
+++ SmartGreenhouse/Validation/GreenhouseCreateDtoValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using SmartGreenhouse.Models.DTOs;
+
+namespace SmartGreenhouse.Validation
+{
+    public class GreenhouseCreateDtoValidator: AbstractValidator<GreenhouseCreateDto>
+    {
+        public GreenhouseCreateDtoValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(100);
+            RuleFor(x => x.Season)
+                .MaximumLength(50);
+            RuleFor(x => x.Location)
+                .MaximumLength(200);
+            RuleFor(x => x.Length)
+                .GreaterThan(0);
+            RuleFor(x => x.Width)
+                .GreaterThan(0);
+            RuleFor(x => x.Height)
+                .GreaterThan(0);
+            RuleFor(x => x.UserId)
+                .GreaterThan(0);
+        }
+    }
+}
